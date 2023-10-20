ff3c92ec553c670b409516a8ef61c55de3db4cc1
evgenii_technion <baziro2345@gmail.com>
2023-10-20 20:31:47 +0300

    Двадцать четвертая итерация динамики. В скрипт main_body.py добавил подсчет работы инерционных сил. Все работает правильно. Инерционные силы равны энергии балки (до удара точно, после удара не проверял).

--- main_body.py
+++ main_body.py
@@ -5,10 +5,12 @@ from matplotlib import pyplot as plt
 from chart_script import *
 import pandas as pd
 import os
+from timeit import default_timer as timer
 
 
 # np.set_printoptions(precision=1)
 def main_body_fun(loc_bar=0.9):
+    timer_start = timer()
     # ---------определяем параметры материала образца----------
     a = 10e-3  # сторона квадратного сечения
     S = a * a  # площадь сечения балки
@@ -99,9 +101,11 @@ def main_body_fun(loc_bar=0.9):
     full_en_mode = [0] * (2 * MaxNode)  # массив полной энергии мод
     step_plot = 1000  # каждый 200ый шаг выводим графики
     number_mode_plot = 10  # количество мод, которое выводим на графиках
+    full_en_lst = []  # массив полной энергии стержня, кин + потен
+    earthquake_en_lst = []  # массив энергии, переданной балке через силы инерции
 
     fig, axs = plt.subplots(3, 2)
-    plt.subplots_adjust(wspace=0.4, hspace=0.7)
+    plt.subplots_adjust(wspace=0.4, hspace=0.8)
     # fig.suptitle('ГРАФИКИ')
     axs[0][0].set_title('Beam shape')
     axs[1][0].set_title('Black - Beam end coordinate,\nGreen - Point opposite the barrier.', fontsize=10)
@@ -128,10 +132,10 @@ def main_body_fun(loc_bar=0.9):
 
 
     # ------- EARTHQUAKE ----------------
-    earthquake_time_step, earthquake_all_data = open_file_earthquake_data()  # при землетрясении
+    earthquake_time_step, earthquake_all_data = open_file_earthquake_data()  # записываем шаг акселерограммы и данные землетрясения
     normal_fr = np.sqrt(2688.5 / 244.98707749566427)  # нормализуем землетрясение в Кобе, сужаем акселлелограмму, что бы несущая частота совпала с первой частотой колебания балки
-    earthquake_time_step = earthquake_time_step / normal_fr
-    earthquake_time_lst = np.linspace(0, (len(earthquake_all_data) - 1) * earthquake_time_step, len(earthquake_all_data))
+    earthquake_time_step = earthquake_time_step / normal_fr  # нормализация через изменение шага по времени акселерограммы
+    earthquake_time_lst = np.linspace(0, (len(earthquake_all_data) - 1) * earthquake_time_step, len(earthquake_all_data))  # нормализированный массив времени для акселерограммы
 
     # вырезаем из акселлелограммы только интересующий нас интервал
     # -------------------
@@ -143,14 +147,15 @@ def main_body_fun(loc_bar=0.9):
                 break
         return id_cut
 
-    time_start = 7.38
-    time_end = 13
+    # time_start = 7.38
+    time_start = 4.3  # начало нужного участка
+    time_end = 13  # конец
     i_start = cut_list(earthquake_time_lst, time_start)
     i_end = cut_list(earthquake_time_lst, time_end)
 
-    earthquake_all_data = earthquake_all_data[i_start:i_end]
+    earthquake_all_data = earthquake_all_data[i_start:i_end]  # оставляем нужный интервал
     # -------------------
-
+    # инвентируем массив данных акселерограммы, что бы читать с конца и постепенно отрезать с помощью pop
     earthquake_data_inverse = earthquake_all_data[::-1]
     accumulated_earthquake_time = 0
     # ---------- END EARTHQUAKE -------------------------
@@ -158,34 +163,40 @@ def main_body_fun(loc_bar=0.9):
     # начинаем цикл по времени
     t = 0
     t_end = 5.62
-    dt_small = 1e-4
-    dt_big = 1e-7
-    MCK_small = global_mass + gamma * dt_small * global_damping + beta * dt_small ** 2 * global_stiffness
-    MCK_small_inv = np.linalg.inv(MCK_small)
-    MCK_big = global_mass + gamma * dt_big * global_damping + beta * dt_big ** 2 * global_stiffness
-    MCK_big_inv = np.linalg.inv(MCK_big)
-    is_near_barrier = False
+
+    dt_lst = [2e-8, 1e-7, 1e-6]  # лист временных шагов, которые будем динамически менять
+    # Начинаем с самого большого шага. Если этим большим шагом зашли вовнутрь барьера, то откываемся на шаг цикла назад и меняем временной шаг на следующий в листе.
+    # Так делаем до тех пор, пока шаг не станет самым маленьким из списка. Потом считаем на этом шаге, но как только балка выйдет из барьера, каждый
+    # следующий шаг делаем на один больше из списка.
+
+    # Для каждого шага по времени заполняем массивы MCK и MCK_inv
+    MCK_lst = [(global_mass + gamma * dt_cur * global_damping + beta * dt_cur ** 2 * global_stiffness) for dt_cur in dt_lst]
+    MCK_inv_lst = [(np.linalg.inv(MCK_cur)) for MCK_cur in MCK_lst]
+
+    time_step_id = len(MCK_inv_lst) - 1  # индекс вревенного шага в листе, который используем в данный момент
+    A_earthquake = 0  # работа инерционных сил землетрясения
     try:
         while t < t_end:
+            dt = dt_lst[time_step_id]
             t += dt
 
-            # MCK = global_mass + gamma * dt * global_damping + beta * dt ** 2 * global_stiffness
-            if is_near_barrier:
-                MCK = MCK_small
-                MCK_inv = MCK_small_inv
-            else:
-                MCK = MCK_big
-                MCK_inv = MCK_big_inv
-
+            # для свободных колебаний
             # global_force = create_global_force(global_force, f_ampl=0)
+
             global_force[point_bar, 0] = 0  # обнуляем силу взаимодействия с барьером
 
             # ------- EARTHQUAKE ----------------
+            # Накапливаем время, пока не перепрыгнем через временной шаг акселерограммы. Когда перепрыгнули, берем следующее значение данных
+            # акселерограммы путем отрезания конца листа данных. Из накопленного времени вычитаем временной шаг акселерограммы.
             accumulated_earthquake_time += dt
+            flag_earthquake_time = True
+            last_value = 0
             if accumulated_earthquake_time >= earthquake_time_step:
-                accumulated_earthquake_time = 0
+                flag_earthquake_time = False
+                accumulated_earthquake_time = (accumulated_earthquake_time - earthquake_time_step)
                 last_value = earthquake_data_inverse.pop()
-                print(last_value)
+                # print(last_value)
+
             global_force = earthquake_force(global_force, earthquake_data_inverse[-1], dm)  # инерционная сила при землетрясении
             # -----------------------------------
 
@@ -202,16 +213,50 @@ def main_body_fun(loc_bar=0.9):
                 # dis_i_before = dis_i[point_bar, 0]
                 pass
 
-            # print(vel_i[MaxNode+1, 0])
+            # Алгоритм НьюМарка
             vel_i1_pred = vel_i + (1 - gamma) * dt * acc_i
             dis_i1_pred = dis_i + dt * vel_i + (0.5 - beta) * dt ** 2 * acc_i
 
-            acc_i1 = np.matmul(MCK_inv,
+            acc_i1 = np.matmul(MCK_inv_lst[time_step_id],
                                global_force - np.matmul(global_damping, vel_i1_pred) - np.matmul(global_stiffness,
                                                                                                  dis_i1_pred))
 
             vel_i1 = vel_i1_pred + gamma * dt * acc_i1
             dis_i1 = dis_i1_pred + beta * dt ** 2 * acc_i1
+
+            # Если зашли в барьер, откатываемся на шаг назад и меняем временной шаг на один меньше по листу
+            # STEP BACK
+            # if jump in barrier make a time step back and decrease time step
+            if (dt > dt_lst[0]) and (dis_i1[point_bar, 0] < 0):
+                # step back
+                t -= dt
+                if flag_earthquake_time:
+                    accumulated_earthquake_time -= dt
+                else:
+                    accumulated_earthquake_time += earthquake_time_step
+                    accumulated_earthquake_time -= dt
+                    earthquake_data_inverse.append(last_value)
+
+                time_step_id -= 1
+                print(t, 'step --> <<')
+
+                continue
+
+            # если вышли из барьра, увеличиваем временной шаг на один больший по листу
+            if (dt < dt_lst[-1]) and (dis_i1[point_bar, 0] > 0):
+                time_step_id += 1
+                print(t, 'step --> >>')
+
+
+            # считаем работу инерционных сил землетрясения
+            if last_value != 0:
+                cur_acc = last_value
+            else:
+                cur_acc = earthquake_data_inverse[-1]
+
+            dis_diff = np.array([dis_i1[i * 2, 0] - dis_i[i * 2, 0] for i in range(MaxNode)])
+            A_earthquake += (-cur_acc * dm) * np.sum(dis_diff)
+            # -----------------------------
 
             dis_i = dis_i1.copy()
             vel_i = vel_i1.copy()
@@ -225,12 +270,12 @@ def main_body_fun(loc_bar=0.9):
 
             if len(time_lst) % step_plot == 0:
                 axs[0][0].set_title('Beam shape')
-                axs[1][0].set_title('Black - Beam end coordinate,\nGreen - Point opposite the barrier.', fontsize=10)
+                axs[1][0].set_title('Black - Beam end coordinate,\nGreen - Point opposite the barrier.', fontsize=10, pad=0)
                 axs[2][0].set_title('VI force')
                 # fig.suptitle('Time = ' + str('%.2f' % t)
                 #              + ' s = ' + str('%.2f' % (t * 1e3)) + ' ms = ' + str('%.2f' % (t * 1e6)) + ' µs')
-                fig.suptitle('Time=' + str('%.2f' % t)
-                             + 's=' + str('%.2f' % (t * 1e3)) + 'ms')
+                fig.suptitle('Real Time = ' + str('%.2f' % t)
+                             + 's = ' + str('%.2f' % (t * 1e3)) + 'ms' + '  (Algorithm Time = ' + str('%.1f' % ((timer() - timer_start) / 60)) + ' min)')
                 axs[0][0].plot(np.linspace(0, L, num=MaxNode), [dis_i[i * 2, 0] for i in range(MaxNode)], 'r',
                             linewidth=1)  # Положение балки
                 axs[0][0].plot([L * (point_bar / 2) / (MaxNode - 1)], [dis_i1[point_bar, 0]], 'go', markersize=4)  # Жирная точка середина балки
@@ -253,15 +298,13 @@ def main_body_fun(loc_bar=0.9):
             # ----------- выводим график полной энергии --------
             dis_i_transp = [disp for sublist in dis_i for disp in sublist]
             vel_i_transp = [vel for sublist in vel_i for vel in sublist]
-            cur_kin_en = 1 / 2 * np.dot(vel_i_transp, np.dot(global_mass, vel_i)) + 1 / 2 * np.dot(dis_i_transp, np.dot(global_stiffness, dis_i))
-            en_kin_lst.append(cur_kin_en)
+            full_cur_en = 1 / 2 * np.dot(vel_i_transp, np.dot(global_mass, vel_i)) + 1 / 2 * np.dot(dis_i_transp, np.dot(global_stiffness, dis_i))
+            full_en_lst.append(full_cur_en)
+            earthquake_en_lst.append(A_earthquake)
             if len(time_lst) % step_plot == 0:
-                # plt.figure(2)
-
-                # plt.plot(time_lst[1:], en_kin_lst, color='k', linewidth=1)
                 axs[0][1].set_title('Total energy')
-                axs[0][1].plot(time_lst[1:], en_kin_lst, color='k', linewidth=1)
-                # axs[0][1].clear()
+                axs[0][1].plot(time_lst[1:], full_en_lst, color='k', linewidth=1)
+                axs[0][1].plot(time_lst[1:], earthquake_en_lst, color='r', linestyle='--', linewidth=1)
             # # --------------------------------------------------------
 
             # # ----------- выводим график модальный скоростей --------
@@ -291,7 +334,7 @@ def main_body_fun(loc_bar=0.9):
             # plt.figure(3)
             if len(time_lst) % step_plot == 0:
                 df = pd.DataFrame(full_en_mode, columns=['origin'], index=range(1, len(modal_vel_i_transp) + 1))
-                axs[1][1].set_title('Energy distribution over modes\nMax energy = {} %'.format(round(max(df.origin[:number_mode_plot]) / sum(df.origin[:number_mode_plot]) * 100)), fontsize=10)
+                axs[1][1].set_title('Energy distribution over modes\nMax energy = {} %'.format(round(max(df.origin[:number_mode_plot]) / sum(df.origin[:number_mode_plot]) * 100)), fontsize=10, pad=0)
                 # axs[1][1].bar(df.index[:number_mode_plot], df.origin[:number_mode_plot])
                 axs[1][1].bar(np.arange(1, number_mode_plot - 1), df.origin[:number_mode_plot].iloc[[1, 2, 3, 4, 6, 7, 8, 9]])
                 # axs[1][1].axis([0, df.index[number_mode_plot], 0, 1e-3])
@@ -329,14 +372,6 @@ def main_body_fun(loc_bar=0.9):
             # df.origin.plot.bar(rot=0, log=True)
             # # --------------------------------------------------------
 
-            # if 10 * dis_i[point_bar, 0] <= -delta:
-            if dis_i[point_bar, 0] <= (start_def_loc_bar / 40):
-                dt = dt_small  # если рядом с барьером
-                is_near_barrier = True
-                # print('little step')
-            else:
-                dt = dt_big
-                is_near_barrier = False
 
             # # каждые сколько-то шагов записываем значения амплитуд колебаний на рассматриваемый частотах в файл
             # if len(time_lst) % 10 == 0:
